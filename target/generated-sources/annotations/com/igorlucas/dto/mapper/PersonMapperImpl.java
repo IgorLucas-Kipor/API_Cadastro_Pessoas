b11b6941b9cb689ed579032958a157d762bfe010
Igor Lucas <igorescritor@hotmail.com>
2021-06-17 18:24:23 -0300

    Reestruturando parte do código

--- target/generated-sources/annotations/com/igorlucas/dto/mapper/PersonMapperImpl.java
+++ target/generated-sources/annotations/com/igorlucas/dto/mapper/PersonMapperImpl.java
@@ -7,12 +7,12 @@ import com.igorlucas.entities.Person.PersonBuilder;
 import com.igorlucas.entities.Phone;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.Generated;
+import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-17T17:59:18-0300",
-    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 15.0.2 (Oracle Corporation)"
+    date = "2021-06-17T18:21:01-0300",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.3.1200.v20200916-0645, environment: Java 15.0.2 (Oracle Corporation)"
 )
 public class PersonMapperImpl implements PersonMapper {
 
